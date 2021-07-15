58e1cf68fcf36afa8931853ff09122f3b30cea7e
Pawel <pszkliniarz@gmail.com>
2021-07-15 18:11:24 +0200

    Comment delete feature
    Only training author can delete comments for his training

--- easy_trainer_v1/workouts/urls.py
+++ easy_trainer_v1/workouts/urls.py
@@ -9,21 +9,23 @@ from .views import (
     CommentCreateView,
     RatingCreateView,
     ImageCreateView,
-    VideoCreateView
+    VideoCreateView,
+    CommentDeletetView
 )
 from . import views
 
 urlpatterns = [
     path('', TrainingListView.as_view(), name='workouts-home'),
+    path('about/', views.about, name='workouts-about'),
+
     path('user/<str:username>', UserTrainingListView.as_view(), name='user-trainings'),
-
-    path('training/<int:pk>/', TrainingDetailtView.as_view(), name='training-detail'),
-    path('training/new/', TrainingCreateView.as_view(), name='training-create'),
     
+    path('training/<int:pk>/', TrainingDetailtView.as_view(), name='training-detail'),
 
+    path('training/new/', TrainingCreateView.as_view(), name='training-create'),
     path('training/<int:pk>/update/', TrainingUpdateView.as_view(), name='training-update'),
     path('training/<int:pk>/delete/', TrainingDeletetView.as_view(), name='training-delete'),
-    path('about/', views.about, name='workouts-about'),
+    path('training/<int:pk>/comment_delete/<int:comment_pk>/', CommentDeletetView.as_view(), name='comment-delete'),
 
     path('training/<int:pk>/comment/', CommentCreateView.as_view(), name='add_comment'),
     path('training/<int:pk>/rating/', RatingCreateView.as_view(), name='add_rating'),
